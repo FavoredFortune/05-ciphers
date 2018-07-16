d953d559d4652de655d75d780108e81453d2e8ad
Suzanne Richman <suzanne@favoredfortune.com>
2018-07-16 13:33:35 -0700

    main messaging working all but catch error message. starting work on cipher method

--- src/Main.java
+++ src/Main.java
@@ -3,57 +3,74 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int encode = 1;
-        int decode = 2;
-        int plainText = 1;
-        int rot13 = 2;
-        int caeser = 3;
-        int keyword =5;
+        //First part of interaction with user - choose encode or decode
+        System.out.println("Welcome to this encoding program.");
+        System.out.println("Select your operation");
+        System.out.println("by typing just the number of your choice");
+        System.out.println("1: encode");
+        System.out.println("2: decode");
+        String answer = scanner.nextLine();
+        codeChoice(answer);
 
-//        Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to this encoding program.\n Select your operation\n" + "1: encode"+"\n" + decode +
-                "\n" + "by typing just the number of your choice" );
-        String input = scanner.nextLine();
-        //turn string into integer for boolean test to route user to coorrect choice
-        //source for help https://www.mkyong.com/java/java-convert-string-to-int/
-        int inputResult = Integer.parseInt(input);
-         try{
-           if(inputResult == 1 ){
-               System.out.println("Your choice: 1 encode");
-           } else if(inputResult == 2 )){
-               System.out.println("Your choice: 2 decode");
-           }
-           catch (InaccessibleObjectException e){
-                 System.out.println("Sorry,  I don't understand your input. \n" + "Please enter only the number 1 or 2 " + "and try again\n");
-             }
-
-        System.out.println("Select your cipher\n" + "1: Plain Text Cipher\n" + "2: ROT13 Cipher\n" + "3: Caesar Shift" +
-                " Cipher\n" + "4: Keyword Cipher\n" +"by entering just the number of your choice\n");
-
-        input = scanner.nextLine();
-             //turn string into integer for boolean test to route user to coorrect choice
-             //source for help https://www.mkyong.com/java/java-convert-string-to-int/
-              inputResult = Integer.parseInt(input);
-         try{
-             if (inputResult == 1 ) {
-                 System.out.println("Your choice: 1: Plain Text Cipher");
-             } else if(inputResult == 2){
-                 System.out.println("Your choice: 2: ROT13 Cipher");
-             }
-             else if(input == 3){
-                 System.out.println("Your choice: 3: Caesar Shift");
-             }
-             else if(input == 4){
-                 System.out.println("Your choice: 4: Keyword Cipher");
-             }
-         }catch (InaccessibleObjectException e){
-                 System.out.println("Sorry,  I don't understand your input. \n" + "Please enter only the number of " +
-                         "your choice: 1, 2, 3 or 4.\n");
-             }
-         }
+        //Second part of interaction with user - choose cipher
+        System.out.println("Select your cipher");
+        System.out.println("1: Plain Text Cipher");
+        System.out.println("2: ROT13 Cipher" );
+        System.out.println("3: Caesar Shift Cipher");
+        System.out.println("4: Keyword Cipher");
+        System.out.println("by entering just the number of your choice");
+        String response = scanner.nextLine();
+        cipherChoice(response);
     }
 
     //help from http://pages.cs.wisc.edu/~hasti/cs368/JavaTutorial/NOTES/JavaIO_Scanner.html to learn how to
     // accept input from the console
     private static Scanner scanner = new Scanner( System.in );
+
+    private static String hunh = "Sorry,  I don't understand your input." +
+            "\nPlease enter only he number and try again.\n";
+
+    public static int codeChoice(String answer) {
+
+        //turn string into integer for boolean test to route user to coorrect choice
+        //source for help https://www.mkyong.com/java/java-convert-string-to-int/
+        int codeChoice = Integer.parseInt(answer);
+        try {
+            if (codeChoice == 1) {
+                System.out.println("Your choice: 1 encode");
+            } else if (codeChoice == 2) {
+                System.out.println("Your choice: 2 decode");
+            }
+        } catch (InaccessibleObjectException e) {
+            System.err.println("Inaccessible Object Exception." + e.getMessage());
+            System.out.println(hunh);
+        }
+        return codeChoice;
+    }
+
+    public static int cipherChoice(String response){
+
+        //turn string into integer for boolean test to route user to coorrect choice
+        //source for help https://www.mkyong.com/java/java-convert-string-to-int/
+        int cipherChoice = Integer.parseInt(response);
+        try{
+            if (cipherChoice == 1 ) {
+                System.out.println("Your choice: 1: Plain Text Cipher");
+            } else if(cipherChoice == 2){
+                System.out.println("Your choice: 2: ROT13 Cipher");
+            }
+            else if(cipherChoice == 3){
+                System.out.println("Your choice: 3: Caesar Shift");
+            }
+            else if(cipherChoice == 4){
+                System.out.println("Your choice: 4: Keyword Cipher");
+            }
+        } catch (InaccessibleObjectException e){
+            System.err.println("Inaccessible Object Exception." + e.getMessage());
+            System.out.println(hunh);
+        }
+        return cipherChoice;
+    }
+
+
 }
--- src/ciphers/Cipher.java
+++ src/ciphers/Cipher.java
@@ -3,6 +3,21 @@ package ciphers;
 public class Cipher {
     public static void main(String[] args) {
 
+        String encode;
+        String decode;
+        String cipherGreeting = "You're encoding with the Plain Text Cipher.";
+        System.out.println(cipherGreeting);
+
+        if (codeChoice() = 1){
+            System.out.println("plain text: ");
+            String plain = scanner.nextLine();
+            
+
+            encode(codeChoice);
+        }
+
+
+
     }
     public Cipher(){
         public static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
@@ -12,7 +27,7 @@ public class Cipher {
         }
         public String decode(String payload){
         }
-        protected String replaceCharacters(String payload, String source, String target){
+        protected String replaceCharacters (String payload, String source, String target){
         }
 
     }
